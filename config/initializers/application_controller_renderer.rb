dc4edce623e47cc0740c7a2971d7d979e43159ef
伊藤脩一郎 <itouosamuichirou@itoushuuichirounoMacBook-puro.local>
2020-04-13 23:33:30 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '87433b6dc97bb02331fea2e445c58ab575afddb944cb3c295de75d9c13f715da802be7ce5e726ebd134f9608905105562c5f0c2b5513a9bc4d866def2544dcf9'
